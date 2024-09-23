6e3c74f5436f889a3704ad2969919848414a0e63
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-09-23 14:19:14 -0300

    feat/mongo-models, modelo das tabelas do mongoDB

---
+++ src/models/cart.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+export default Cart;
