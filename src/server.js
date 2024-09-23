c1746b98e6f1a37f8c6c8e0dab91c9a39c11a097
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-09-23 14:03:34 -0300

    feat/mongo, configuração para esta mudança realizada com sucesso.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
-import { appPort } from "./env/envoriment";
+import { appPort } from "./env/envoriment.js";
+import mongoConfig from "./config/mongo.config.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+mongoConfig();
 
 app.listen(appPort, () =>
   console.log(`Server running at http://localhost:${appPort}`),
