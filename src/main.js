88a659d194e92b11dc28ea710f64e12d1472dceb
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2025-01-20 10:42:18 -0300

    feat/vercel-and-swagger, implementação da documentação da api e da infra dela.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-import app from './index.js';
+import express from "express";
+const app = express();
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" with { type: "json" };
+import { appPort } from "./env/envoriment.js";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (req, res) => {
+  return res.status(200).json("Hello World!");
+});
+
+app.listen(appPort, () => {
+  console.log(`Server running on port http://localhost:${appPort}/docs`);
+});
