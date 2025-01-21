4e51ca9dbb47ac0d60371795c60fe7d9570b35de
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2025-01-21 10:35:15 -0300

    feat/auth-and-auto, implementação da conexção e configuração para conectar com o mongoDB.

--- src/env/envoriment.js
+++ src/env/envoriment.js
@@ -5,3 +5,11 @@ dotenv.config();
 export const appPort = process.env.APP_PORT || 3000;
 
 export const mongoUri = process.env.MONGODB_URI;
+
+export const upstashRedisRestUrl = process.env.UPSTASH_REDIS_REST_URL;
+export const upstashRedisRestToken = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+export const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+export const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
+
+export const nodeEnv = process.env.NODE_ENV;
